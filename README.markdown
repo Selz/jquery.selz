8262ad0fd65e66f20b1f507f642bf32e2aa57539
slavo.lukac <slavo.lukac@communityengine.com>
2013-08-21 09:15:18 +1000

    plugin

---
+++ README.markdown
@@ -0,0 +1,36 @@
+# jQuery.selz
+
+## Open [Selz.com](https://selz.com) product hyperlinks in overlay to let your customers complete their purchase directly onto your site.
+
+### Examples
+
+See bundled `index.html` and `index.theme.html` file for examples.
+
+### Usage
+
+1. include jQuery and `jquery.selz.min.js` files.
+
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+        <script src="src/jquery.selz.min.js" type="text/javascript"></script>
+
+2. include `jquery.selz.min.css` file or add your stylesheet.
+
+        <link href="src/jquery.selz.min.css" type="text/css" rel="stylesheet">
+
+3. add your colors to customize overlay.
+
+        <script type="text/javascript">
+            $.selz({
+                buttonBg: "#ff0000",
+                buttonText: "#fff"
+            });
+        </script>
+
+4. add hyperlinks to your [Selz.com](https://selz.com) products into `<body>`.
+
+        <a href="http://selz.co/19HmDA4">Buy now</a>
+
+
+### Licensed under the MIT
+
+[License text](http://www.opensource.org/licenses/mit-license.php)
---
+++ src/jquery.selz.js
@@ -0,0 +1,70 @@
+﻿// NOTE: works with _$elz embeded scripts
+// require jQuery
+
+// mock _$elz global variable
+var _$elz = _$elz || {};
+
+!function ($) {
+    // plugin config
+    var config = {
+        domain: "https://selz.com",
+        settings: { colors: null }
+    };
+
+    // mock _$elz modal
+    _$elz.m = _$elz.m || {
+        s: {
+            src: config.domain + "/assets/js/embed/modal.js"
+        }
+    };
+
+    function openOverlay() {
+        $.getJSON(config.domain + "/embed/itemdata/?itemUrl=" + $(this).attr("href") + "&callback=?", function (res) {
+            _$elz.m.open(res.Url, null);
+        });
+
+        return false;
+    }
+
+    function setTheme($e) {
+        var msg = null,
+           e = $e.originalEvent;
+
+        // liosten only selz messages
+        if (e.origin !== config.domain || config.settings.colors === null) {
+            return;
+        }
+
+        if (typeof e.message !== "undefined") {
+            msg = e.message;
+        } else if (typeof e.data !== "undefined") {
+            msg = e.data;
+        }
+
+        if (/modal_colors/i.test(msg)) {
+            var reply = config.settings.colors.buttonText + "," + config.settings.colors.buttonBg;
+            e.source.postMessage(reply, config.domain);
+        }
+    }
+
+    // listeners
+    function listeners() {
+        $(document.body).on("click", 'a[href^="http://selz.co/"]', openOverlay);
+        $(window).on("message", setTheme);
+    }
+
+    // preload _$elz modal script if needed
+    if (typeof _$elz.m.open === "undefined") {
+        $.getScript(_$elz.m.s.src, function (data, textStatus, jqxhr) {
+            listeners();
+        });
+    } else {
+        listeners();
+    }
+
+    // plugin
+    $.selz = function (options) {
+        config.settings.colors = $.extend({}, config.settings.colors, options);
+    };
+
+} (window.jQuery);
