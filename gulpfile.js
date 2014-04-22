6cb8b87f89ab9b88422a63017a541b35d1c68712
slavo.lukac <slavo.lukac@communityengine.com>
2014-04-22 14:58:00 +1000

    publish to cdn task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-﻿var gulp = require('gulp');
-
+﻿var fs = require('fs');
+var gulp = require('gulp');
 var gutil = require('gulp-util');
 var changed = require('gulp-changed');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
+var rename = require("gulp-rename");
+var awspublish = require('gulp-awspublish');
 
 var paths = {
 	scripts: ['src/jquery.selz.js'],
@@ -36,3 +38,39 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['scripts', 'css', 'watch']);
+
+
+/* publish task */
+
+var aws = {
+	"key": "",
+	"secret": "",
+	"bucket": ""
+};
+
+try {
+	aws = JSON.parse(fs.readFileSync('./aws.json'));
+} catch (e) {
+}
+
+gulp.task('upload', function () {
+    var publisher = awspublish.create(aws);
+    var maxAge = 31536000; // seconds 1 year
+
+    var headers = {
+        "Cache-Control": "max-age=" + maxAge,
+        "Expires": new Date(Date.now() + (maxAge * 1000)).toUTCString(),
+        "Content-Encoding": "gzip"
+    };
+
+    return gulp.src('src/jquery.selz.min**')
+		.pipe(rename(function (path) {
+		    path.dirname += '/jqueryselz';
+		}))
+		.pipe(awspublish.gzip())
+		.pipe(publisher.publish(headers))
+    //.pipe(publisher.cache())
+		.pipe(awspublish.reporter());
+});
+
+gulp.task('publish', ['scripts', 'css', 'upload']);
