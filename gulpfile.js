49f0c23358ec4a2aff22326384daa5b408343922
Sam Potts <sam@selz.com>
2015-08-07 16:01:25 +1000

    Use purely package.json for versioning
    Automatic docs update, package.json used for versioning

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,94 @@
-﻿var fs = require('fs');
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var changed = require('gulp-changed');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var less = require('gulp-less');
-var minifyCss = require('gulp-minify-css');
-var rename = require("gulp-rename");
-var awspublish = require('gulp-awspublish');
-var prefixer = require('gulp-autoprefixer');
+﻿// ==========================================================================
+// Gulp build script
+// ==========================================================================
+/*global require, __dirname*/
+/*jshint -W079 */
 
-var paths = {
-	scripts: ['src/jquery.selz.js'],
-	css: ['src/jquery.selz.less'],
-	build: 'src'
+var fs 			= require("fs"),
+	gulp 		= require("gulp"),
+	gutil 		= require("gulp-util"),
+	concat 		= require("gulp-concat"),
+	uglify 		= require("gulp-uglify"),
+	less 		= require("gulp-less"),
+	minifyCss 	= require("gulp-minify-css"),
+	rename 		= require("gulp-rename"),
+	awspublish 	= require("gulp-awspublish"),
+	prefixer 	= require("gulp-autoprefixer"),
+	replace     = require("gulp-replace");
+
+// Load json
+function loadJSON(path) {
+    return JSON.parse(fs.readFileSync(path));
+}
+
+// Set paths
+var root = __dirname,
+	paths = {
+	scripts: 	["src/jquery.selz.js"],
+	css: 		["src/jquery.selz.less"],
+	build: 		"src",
+	docs: 		["README.markdown", "*.html"],
+	cdn: {
+		root: 	"cdn.selz.com/jquery"
+	}
 };
 
-gulp.task('scripts', function() {
+// Default development tasks
+// ------------------------------------------
+gulp.task("scripts", function() {
 	return gulp.src(paths.scripts)
-		//.pipe(changed(paths.build, { extension: '.js' }))
 		.pipe(uglify())
-		.pipe(concat('jquery.selz.min.js'))
+		.pipe(concat("jquery.selz.min.js"))
 		.pipe(gulp.dest(paths.build));
 });
 
-gulp.task('css', function() {
+gulp.task("css", function() {
 	return gulp.src(paths.css)
-		//.pipe(changed(paths.build, { extension: '.less' }))
-		.pipe(less()).on('error', gutil.log)
-		.pipe(concat('jquery.selz.min.css'))
+		.pipe(less()).on("error", gutil.log)
+		.pipe(concat("jquery.selz.min.css"))
 		.pipe(prefixer(["last 2 versions", "> 1%", "ie 9"], { cascade: true }))
 		.pipe(minifyCss())
 		.pipe(gulp.dest(paths.build));
 });
 
-gulp.task('watch', function() {
-	gulp.watch(paths.scripts, ['scripts']);
-	gulp.watch(paths.css, ['css']);
+gulp.task("watch", function() {
+	gulp.watch(paths.scripts, ["scripts"]);
+	gulp.watch(paths.css, ["css"]);
 });
 
-gulp.task('default', ['scripts', 'css', 'watch']);
+gulp.task("default", ["scripts", "css", "watch"]);
 
+// Publish to production
+// ------------------------------------------
+// Fetch info from JSON
+var package = loadJSON("package.json"),
+	aws 	= loadJSON("aws.json"),
+	version = package.version,
+	cdnpath = new RegExp(paths.cdn.root + "\/(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)","gi");
 
-/* publish task */
+// Update version references
+gulp.task("docs", function () {
+    console.log("Updating version references to " + version);
 
-var aws = {
-	"key": "",
-	"secret": "",
-	"bucket": ""
-};
+    gulp.src(paths.docs)
+        .pipe(replace(cdnpath, paths.cdn.root + "/" + version))
+        .pipe(gulp.dest(root));
+});
 
-try {
-	aws = JSON.parse(fs.readFileSync('./aws.json'));
-} 
-catch (e) {
-}
-
-gulp.task('upload', function () {
-    var publisher = awspublish.create(aws);
-    var maxAge = 31536000; // seconds 1 year
+// Upload to S3
+gulp.task("upload", function () {
+    var publisher = awspublish.create(aws),
+    	maxAge = 31536000; // seconds 1 year
 
     var headers = {
         "Cache-Control": "max-age=" + maxAge,
-        "Expires": new Date(Date.now() + (maxAge * 1000)).toUTCString(),
         "Content-Encoding": "gzip",
         "Vary": "Accept-Encoding"
     };
 
-    return gulp.src('src/jquery.selz.min**')
+    return gulp.src("src/jquery.selz.min**")
 		.pipe(rename(function (path) {
-		    path.dirname += '/jquery/1.0.6';
+		    path.dirname += "/jquery/" + version;
 		}))
 		.pipe(awspublish.gzip())
 		.pipe(publisher.publish(headers))
@@ -77,4 +96,4 @@ gulp.task('upload', function () {
 		.pipe(awspublish.reporter());
 });
 
-gulp.task('publish', ['scripts', 'css', 'upload']);
+gulp.task("publish", ["scripts", "css", "upload", "docs"]);
